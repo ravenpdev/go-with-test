b95546a2c9846404f42e03de501f8f197c548a04
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2025-02-11 18:59:57 +0800

    choir: display proper greeting based on the language
    1. write test
    2. declaring functions, with arguments and return types
    3. if, const switch
    4. declaring variables and constant

--- 01-hello-world/hello.go
+++ 01-hello-world/hello.go
@@ -2,16 +2,38 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Holla, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+
 	if name == "" {
-		return englishHelloPrefix + "World"
+		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+// private function in go start with lowercase letter
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("raven"))
+	fmt.Println(Hello("raven", "Spanish"))
 }
