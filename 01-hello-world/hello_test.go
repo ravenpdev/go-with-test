2c385f3ced9288444b3f7e61f108e1eda625e779
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2025-02-11 18:59:57 +0800

    chore: display proper greeting based on the language
    1. write test
    2. declaring functions, with arguments and return types
    3. if, const switch
    4. declaring variables and constant

--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -13,15 +13,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("raven")
+		got := Hello("raven", "")
 		want := "Hello, raven"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people in spanish", func(t *testing.T) {
+		got := Hello("raven", "Spanish")
+		want := "Holla, raven"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to people in french", func(t *testing.T) {
+		got := Hello("raven", "French")
+		want := "Bonjour, raven"
 
 		assertCorrectMessage(t, got, want)
 	})
