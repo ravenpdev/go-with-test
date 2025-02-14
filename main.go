36a9dd72a364e0f0d67b942840eaa5285dcb041a
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2025-02-14 19:58:06 +0800

    chore: array and slices

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	sum := 0
+
+	for _, value := range numbers {
+		sum += value
+	}
+
+	fmt.Println(sum)
+
+	cube := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		cube[i] = make([]int, 3)
+		for x := 0; x < 3; x++ {
+			if i%2 != 0 {
+				cube[i][x] = 1
+			} else {
+				cube[i][x] = 0
+			}
+		}
+	}
+
+	fmt.Printf("%v", cube)
+
+	// d := []byte{'r', 'o', 'a', 'd'}
+	// e := d[2:]
+	// fmt.Printf("%v\n", d)
+	// fmt.Printf("%v\n", e)
+	// e[1] = 'm'
+	// fmt.Printf("%v\n", d)
+	// fmt.Printf("%v\n", e)
+}
