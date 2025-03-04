81d33dab4245a1ee8227442ef48aa853bd22e3a3
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2025-03-04 08:28:49 +0800

    chor: reflection

--- 12-reflection/reflections/walk_test.go
+++ 12-reflection/reflections/walk_test.go
@@ -68,6 +68,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
 	}
 
 	for _, test := range cases {
@@ -81,6 +89,74 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Cow":   "Moo",
+			"Sheep": "Baa",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Kristine"}
+			aChannel <- Profile{32, "Raven"}
+
+			defer close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Kristine", "Raven"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with functions", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Kristine"}, Profile{32, "Raven"}
+		}
+
+		var got []string
+		want := []string{"Kristine", "Raven"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %v to contain %q but it didn't", haystack, needle)
 	}
 }
 
